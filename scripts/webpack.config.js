cbc7c62efce3bdf35d797d1ecd2f984215076fcb
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2019-08-28 07:36:14 +0300

    css config added

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,15 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { resolve } = require('path');
 
 module.exports = () => {
     
     return {
         mode: 'development',
+        entry: resolve(__dirname, '../src'),
+        output: {
+            path: resolve(__dirname, '../dist'),
+            filename: 'bundle.js'
+        },
         devtool: false,
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [ 'style-loader','css-loader' ]
+                }
+            ]
+        },
         plugins: [
             new HtmlWebpackPlugin({
                 title: 'Dashboard',
                 template: './static/template.html'
-            })
+            }),
+            new CleanWebpackPlugin()
         ]
     };
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import './theme/style.css';
+
 console.log('<-------------- hello from index.js');
+
+function addChild() {
+    const element = document.createElement('div');
+    element.innerHTML = 'Text';
+
+    return element;
+}
+
+document.body.appendChild(addChild());
