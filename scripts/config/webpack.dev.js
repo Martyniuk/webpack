0ac8bd3a40dbe6e731fd64b3f9de7abe60fc3f9c
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2019-09-10 23:37:42 +0300

    feat(webpack): added dev-utils, dynamik port generator

--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -1,16 +1,21 @@
 // Core
-const { HotModuleReplacementPlugin } = require('webpack');
-const merge = require('webpack-merge');
-
+const { HotModuleReplacementPlugin } = require("webpack");
+const merge = require("webpack-merge");
+const { choosePort } = require("react-dev-utils/WebpackDevServerUtils");
 // Instruments
-const { SOURCE } = require('../constants');
-const getCommonConfig = require('./webpack.common');
+const { SOURCE, HOST, PORT } = require("../constants");
+const getCommonConfig = require("./webpack.common");
 
-module.exports = () => {
+module.exports = async () => {
+  const suggestedPort = await choosePort(HOST, PORT);
 
-    return merge(getCommonConfig(), {
-        mode:    'development',
-        entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
-        plugins: [ new HotModuleReplacementPlugin() ]
-    });
+  return merge(getCommonConfig(), {
+    mode: "development",
+    entry: ["webpack-hot-middleware/client?reload=true&quiet=true", SOURCE],
+    devServer: {
+      host: HOST,
+      port: suggestedPort
+    },
+    plugins: [new HotModuleReplacementPlugin()]
+  });
 };
--- scripts/start.js
+++ scripts/start.js
@@ -8,38 +8,43 @@ const chalk = require("chalk");
 const getDevConfig = require("./config/webpack.dev");
 
 // Constants
-const { HOST, PORT } = require("./constants");
+// const { HOST, PORT } = require("./constants");
 
-const compiler = webpack(getDevConfig());
+(async () => {
+  const config = await getDevConfig();
+  const {
+    devServer: { host, port }
+  } = config;
+  const compiler = webpack(config);
+  const server = new devServer(compiler, {
+    // lifecycle hook of devserver, gives chance to register middleware
+    host,
+    port,
+    // optimizes work with SPA, return page to index.js by default
+    historyApiFallback: true,
+    // Error will be shown inside Browser page
+    overlay: true,
+    // cleans up output of console
+    quiet: true,
+    // no output at ALL
+    clientLogLevel: "none",
+    // total Silence
+    noInfo: true,
+    after: app => {
+      app.use(
+        hot(compiler, {
+          // additional options can be given
+          log: false
+        })
+      );
+    }
+  });
 
-const server = new devServer(compiler, {
-  // lifecycle hook of devserver, gives chance to register middleware
-  host: HOST,
-  port: PORT,
-  // optimizes work with SPA, return page to index.js by default
-  historyApiFallback: true,
-  // Error will be shown inside Browser page
-  overlay: true,
-  // cleans up output of console
-  quiet: true,
-  // no output at ALL
-  clientLogLevel: "none",
-  // total Silence
-  noInfo: true,
-  after: app => {
-    app.use(
-      hot(compiler, {
-        // additional options can be given
-        log: false
-      })
+  server.listen(port, host, () => {
+    console.log(
+      `${chalk.greenBright("⟹  Server is listening on")} ${chalk.blueBright(
+        `http://${host}:${port}`
+      )}`
     );
-  }
-});
-
-server.listen(PORT, HOST, () => {
-  console.log(
-    `${chalk.greenBright("⟹  Server is listening on")} ${chalk.blueBright(
-      `http://${HOST}:${PORT}`
-    )}`
-  );
-});
+  });
+})();
