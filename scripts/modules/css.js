c9d368a6221270ad22b2946d748221b694f18f11
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2019-09-26 19:50:45 +0300

    feat: css refactoring in progress

--- scripts/modules/css.js
+++ scripts/modules/css.js
@@ -1,4 +1,5 @@
 // Core
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const env = require("postcss-preset-env");
 const fontMagician = require("postcss-font-magician");
 // Instrumets
@@ -36,4 +37,74 @@ exports.loadCSS = () => {
       }
     ]
   };
+};
+
+exports.loadPostCSS = ({ sourceMap = false } = { sourceMap: false }) => {
+  console.log(chalk.greenBright("< \t << ---- PostCSS loader in loadCSS"));
+
+  const plugins = [
+    env({
+      stage: 0
+    }),
+    fontMagician({
+      protocol: "https:" // what it does? - generates fontface automaticaly, magic?
+    })
+  ];
+
+  return {
+    loader: "postcss-loader",
+    optnios: {
+      plugins,
+      sourceMap
+    }
+  };
+};
+
+exports.loadCSS = ({ sourceMap = false } = { sourceMap: false }) => {
+  console.log(chalk.blueBright("< \t << ---- CSS loader in loadCSS"));
+
+  return {
+    loader: "css-loader",
+    optnios: {
+      modules: true,
+      importLoaders: 1,
+      localIdentName: "[path][name]__[local]--[hash:base64:5]",
+      sourceMap
+    }
+  };
+};
+
+exports.loadDevCSS = () => {
+  console.log(chalk.blueBright("< ---- CSS Loaded"));
+
+  return {
+    test: /\.css$/,
+    use: [
+      "style-loader",
+      loadCSS({ sourceMap: true }), // css-loader
+      loadPostCss({ sourceMap: true /*minimize: false*/ }) // postcss-loader
+    ]
+  };
+};
+
+exports.loadProdCSS = () => {
+  console.log(chalk.blueBright("< ---- CSS Loaded"));
+
+  return {
+    test: /\.css$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      "cache-loader",
+      loadCSS({ sourceMap: false }), // css-loader
+      loadPostCss({ sourceMap: false, minimize: true }) // postcss-loader
+    ]
+  };
+};
+
+/* ----------------------------- PLUGINS ------------------------------------*/
+exports.connectMiniCssExtractPlugin = () => {
+  return new MiniCssExtractPlugin({
+    filename: "css/styles.css"
+    // chunkFilename: "css/style.css"
+  });
 };
